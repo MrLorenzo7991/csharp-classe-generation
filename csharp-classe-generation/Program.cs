35bd6a56c012e996576df140860ea7a2f99944dc
Lorenzo <lorenzo.miccio97@gmail.com>
2022-04-06 14:29:04 +0200

    Add project files.

---
+++ csharp-classe-generation/Program.cs
@@ -0,0 +1,16 @@
+﻿/*--------------------VARIABILI--------------------*/
+int numeroPartecipanti = 10;
+string[] nomi = new string[numeroPartecipanti];
+string[] cognomi = new string[numeroPartecipanti]; 
+int[] eta = new int[numeroPartecipanti];
+
+/*--------------------FUNZIONI--------------------*/
+void StampaArrayDiStringhe(string[] arrayDiStringhe)
+{
+    Console.Write("[");
+    for (int i = 0; i < arrayDiStringhe.Length - 1 ; i++)
+    {
+        Console.Write(arrayDiStringhe[i] + ", ");
+    }
+    Console.WriteLine(arrayDiStringhe[arrayDiStringhe.Length - 1] + "]");
+}
