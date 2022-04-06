240208d882a3e3cc29767cd6d22526417bbd3604
Lorenzo <lorenzo.miccio97@gmail.com>
2022-04-06 15:33:51 +0200

    Completato aggiungiAlunno

--- csharp-classe-generation/Program.cs
+++ csharp-classe-generation/Program.cs
@@ -1,8 +1,9 @@
 ﻿/*--------------------VARIABILI--------------------*/
-int numeroPartecipanti = 10;
-string[] nomi = new string[numeroPartecipanti];
-string[] cognomi = new string[numeroPartecipanti]; 
-int[] eta = new int[numeroPartecipanti];
+int numeroPartecipantiMassimi = 10;
+int numeroPartecipantiAttuali = 0;
+string[] nomi = new string[numeroPartecipantiMassimi];
+string[] cognomi = new string[numeroPartecipantiMassimi]; 
+int[] eta = new int[numeroPartecipantiMassimi];
 
 /*--------------------FUNZIONI--------------------*/
 void StampaArrayDiStringhe(string[] arrayDiStringhe)
@@ -24,3 +25,53 @@ void StampaEtaAlunni(int[] arrayDiInteri)
     }
     Console.WriteLine(arrayDiInteri[arrayDiInteri.Length - 1] + "]");
 }
+
+void AggiungiAlunno()
+{
+    if (numeroPartecipantiAttuali == numeroPartecipantiMassimi)  //è giusto inserire variabili che non sono presenti in input?
+    {
+        Console.WriteLine("Hai Raggiunto il numero massimo di partecipanti, non puoi aggiungere nuovi alunni.");
+    }
+    else
+    {
+        Console.WriteLine("Inserisci il nome dell'alunno che vuoi aggiungere:");
+        nomi [numeroPartecipantiAttuali] = Console.ReadLine();
+        Console.WriteLine("Inserisci il cognonome dell'alunno che vuoi aggiungere:");
+        cognomi [numeroPartecipantiAttuali] = Console.ReadLine();
+        Console.WriteLine("Inserisci l'età dell'alunno che vuoi aggiungere:");
+        bool flag = false;
+        do
+        {
+            if (int.TryParse(Console.ReadLine(), out int etaCorretto))
+            {
+                eta[numeroPartecipantiAttuali] = etaCorretto;
+                numeroPartecipantiAttuali++;
+                flag = true;
+            }
+            else
+            {
+                Console.WriteLine("Il formato dell'età inserito non è corretto, perfavore riprova.");
+            }
+
+        }while (!flag);
+
+
+    }
+}
+
+StampaArrayDiStringhe(nomi);
+StampaArrayDiStringhe (cognomi);
+StampaEtaAlunni(eta);
+
+AggiungiAlunno();
+
+StampaArrayDiStringhe(nomi);
+StampaArrayDiStringhe(cognomi);
+StampaEtaAlunni(eta);
+
+AggiungiAlunno();
+
+StampaArrayDiStringhe(nomi);
+StampaArrayDiStringhe(cognomi);
+StampaEtaAlunni(eta);
+
